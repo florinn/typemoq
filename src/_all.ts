0297906dffc1af86a189beedd896e81894d0de20
florinn <florin.nitoi@gmail.com>
2016-07-10 23:22:55 +0300

    change test to reference only the output of src

--- src/_all.ts
+++ src/_all.ts
@@ -6,22 +6,6 @@
 /// <reference path='Match/_all.ts' />
 /// <reference path='Proxy/_all.ts' />
 
-/// <reference path='Consts.ts' />
-/// <reference path='CurrentInterceptContext.ts' />
-/// <reference path='GlobalMock.ts' />
-/// <reference path='GlobalScope.ts' />
-/// <reference path='IGlobalMock.ts' />
-/// <reference path='IMock.ts' />
-/// <reference path='InterceptorContext.ts' />
-/// <reference path='InterceptorExecute.ts' />
-/// <reference path='InterceptorSetup.ts' />
-/// <reference path='InterceptorStrategies.ts' />
-/// <reference path='It.ts' />
-/// <reference path='MethodCall.ts' />
-/// <reference path='MethodCallReturn.ts' />
-/// <reference path='Mock.ts' />
-/// <reference path='Times.ts' />
-
 import api     = TypeMoqIntern.Api;
 import error   = TypeMoqIntern.Error;
 import match   = TypeMoqIntern.Match;
--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,6 @@
-﻿/// <reference path='../src/_exports.ts' />
+﻿/// <reference path='../.tmp/src/output.d.ts' />
 
+/// <reference path='../bower_components/DefinitelyTyped/underscore/underscore.d.ts' /> 
 /// <reference path='../bower_components/DefinitelyTyped/node/node.d.ts' /> 
 /// <reference path='../bower_components/DefinitelyTyped/mocha/mocha.d.ts' /> 
 /// <reference path='../bower_components/DefinitelyTyped/chai/chai.d.ts' /> 
@@ -23,7 +24,7 @@ function isNodeJS(): boolean {
 if (isNodeJS()) {
     chai = require('chai');
     _ = require('underscore');
-    typemoq = require('../../../dist/typemoq.js');
+    typemoq = require('../../dist/typemoq.js');
 }
 
 let assert = chai.assert;
