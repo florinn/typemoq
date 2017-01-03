2669f9fdc26788e0148c2e3ffa6b55c537d791e0
florinn <florin.nitoi@gmail.com>
2017-01-03 14:19:26 +0200

    BREAKING CHANGE: drop support for IE9, IE10 due to missing Object.setPrototypeOf

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -23,16 +23,6 @@ module.exports = function (config) {
             base: 'SauceLabs',
             browserName: 'firefox'
         },
-        'SL_IE_9': {
-            base: 'SauceLabs',
-            browserName: 'internet explorer',
-            version: '9'
-        },
-        'SL_IE_10': {
-            base: 'SauceLabs',
-            browserName: 'internet explorer',
-            version: '10'
-        },
         'SL_IE_11': {
             base: 'SauceLabs',
             browserName: 'internet explorer',
